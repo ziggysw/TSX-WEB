7457b2d620522c4b645075604d494df9336e6c2b
Steve Zaretti <kossolax@gmail.com>
2015-12-01 09:48:20 +0100

    clean

--- js/app.js
+++ js/app.js
@@ -4,4 +4,3 @@ var app = angular.module('tsx', ['ngRoute']);
 require("./angular/ctrl/roleplay.js")(app);
 require("./angular/route/roleplay.js")(app);
 require("./angular/directive/roleplay.js")(app);
-require("./angular/filter/roleplay.js")(app);
