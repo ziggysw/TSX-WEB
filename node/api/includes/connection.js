97ad3dbebfea0fee1a2ca026a80f4555155289cd
FiReBlUe <copois.p@gmail.com>
2015-12-03 12:02:42 +0100

    Ajout API connection

---
+++ node/api/includes/connection.js
@@ -0,0 +1,41 @@
+"use strict";
+exports = module.exports = function(server){
+  var ERR = require('node-restify-errors');
+  var moment = require('moment');
+
+/**
+ * @api {get} /connect/auth GetAuth
+ * @apiName GetAuth
+ * @apiGroup Forum
+ * @apiHeader {String} auth Votre cookie de connexion.
+ */
+/*
+
+*/
+server.get('/connect/auth', function (req, res, next) {
+  server.conn.query(server.getAuthSteamID, [req.headers.auth], function(err, row) {
+    if( row.length == 0 ) throw "NotAuthorized";
+    var uid = row[0].user_id;
+    return res.send(req.headers.auth);
+  });
+  next();
+});
+
+/**
+ * @api {get} /connect/check GetCheckConnect
+ * @apiName GetCheckConnect
+ * @apiGroup Forum
+ * @apiHeader {String} auth Votre cookie de connexion.
+ */
+/*
+
+*/
+server.get('/connect/check', function (req, res, next) {
+  server.conn.query(server.getAuthSteamID, [req.headers.auth], function(err, row) {
+    if( row.length == 0 ) throw "NotAuthorized";
+    var uid = row[0].user_id;
+    return res.send("ok");
+  });
+  next();
+});
+};
