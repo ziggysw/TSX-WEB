8d31d88fbfd7152282fcd75d69e866faa4e320a9
Steve Zaretti <kossolax@gmail.com>
2015-12-08 12:24:32 +0100

    passé à 8 connexions

--- node/api/app.js
+++ node/api/app.js
@@ -10,9 +10,7 @@ function Pool(num_conns) {
   for(var i=0; i < num_conns; ++i) {
     var conn = mysql.createConnection(server.sqlConfig);
     conn.connect();
-    conn.on('error', function(err) {
-        setTimeout(handleDisconnect, 5000);
-    });
+    conn.on('error', function(err) {  setTimeout(handleDisconnect, 5000);  });
     this.pool.push(conn);
   }
   this.last = 0;
@@ -28,7 +26,7 @@ Pool.prototype.query = function(a, b, c, d) {
 var server = restify.createServer({key: fs.readFileSync("../www.ts-x.eu.key"), certificate: fs.readFileSync("../www.ts-x.eu.crt"), ca: fs.readFileSync('../intermediate.crt') });
 require('./auth.js')(server);
 
-server.conn = new Pool(16);
+server.conn = new Pool(8);
 server.cache = new NodeCache({ stdTTL: 30, checkperiod: 60 });
 server.restify = restify;
 server.restify.CORS.ALLOW_HEADERS.push('origin');
@@ -37,7 +35,7 @@ server.restify.CORS.ALLOW_HEADERS.push('Accept-Encoding');
 server.restify.CORS.ALLOW_HEADERS.push('Accept-Language');
 
 function handleDisconnect() {
-    server.conn = new Pool(16);
+    server.conn = new Pool(8);
 }
 //handleDisconnect();
 
