253be517f43538cb44e59e6c0233ae8632ee82cb
Steve Zaretti <kossolax@gmail.com>
2016-02-25 16:00:25 +0100

    whoops

---
+++ js/apps.js
@@ -0,0 +1,11 @@
+// sssdn,kaeae
+var app = angular.module('tsx', ['ngRoute', 'dndLists']);
+
+app.config(function($httpProvider) {
+	$httpProvider.defaults.headers.common['auth'] = _md5;
+});
+
+require("./angular/ctrl/roleplay.js")(app);
+require("./angular/route/roleplay.js")(app);
+require("./angular/directive/roleplay.js")(app);
+require("./angular/filter/roleplay.js")(app);
