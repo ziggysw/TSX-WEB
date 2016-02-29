6d57953c6d3b3015a897d4b75243d7803c1ff5e4
Leethium <dev@leethium.fr>
2016-02-29 15:29:30 +0100

    Cache + Gestion Erreurs + /devzone/status GetStatus
    Signed-off-by: Leethium <dev@leethium.fr>

--- node/api/includes/devzone.js
+++ node/api/includes/devzone.js
@@ -23,6 +23,25 @@ exports = module.exports = function(server){
   });
   
   /**
+   * @api {get} /devzone/status GetStatus
+   * @apiName GetStatus
+   * @apiGroup DevZone
+   * @apiHeader {String} auth Votre cookie de connexion.
+   */
+  server.get('/devzone/status', function (req, res, next) {
+    var cache = server.cache.get( req._url.pathname);
+    if( cache != undefined ) { return res.send(cache); }
+    
+    var sql = "SELECT S.stat_id, S.stat_name, S.stat_priority, UNIX_TIMESTAMP(S.stat_date), S.stat_hidden FROM `leeth`.dz_status S ORDER BY stat_hidden ASC,stat_priority DESC";
+    server.conn.query(sql, [], function(err, rows){
+      if( err ) throw err;
+      server.cache.set( req._url.pathname, rows);
+      return res.send(rows);
+    });
+    next();
+  });
+  
+  /**
    * @api {get} /devzone/ticket GetTickets
    * @apiName GetTickets
    * @apiGroup DevZone
@@ -30,6 +49,9 @@ exports = module.exports = function(server){
    */
   server.get('/devzone/ticket', function (req, res, next) {
     dz.user(server, req.headers.auth,function(user){
+      var cache = server.cache.get( req._url.pathname+'-'+user.accesslevel );
+      if( cache != undefined ) { return res.send(cache); }
+      
       var sql = "SELECT S.stat_id, tk_id, tk_title, usr_id, assig_usr_id, tk_desc, tk_showdesc, T.cat_id, tk_url FROM `leeth`.dz_status S "
         +"LEFT JOIN `leeth`.dz_ticket T ON T.stat_id = S.stat_id "
 	      +"LEFT JOIN `leeth`.dz_cat C ON T.cat_id = C.cat_id "
@@ -37,6 +59,8 @@ exports = module.exports = function(server){
 	      +"S.stat_id IN(SELECT * FROM (SELECT S.stat_id FROM `leeth`.dz_status S ORDER BY stat_hidden ASC,stat_priority DESC LIMIT 4) AS temp) "
 	      +"ORDER BY stat_hidden ASC,stat_priority DESC,cat_prio DESC,IFNULL(tk_prio,0) ASC;";
       server.conn.query(sql, [user.accesslevel], function(err, rows){
+        if( err ) throw err;
+        server.cache.set( req._url.pathname+'-'+user.accesslevel , rows);
         return res.send(rows);
       });
     });
